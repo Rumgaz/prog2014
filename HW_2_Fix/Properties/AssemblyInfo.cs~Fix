b2f56458a69c4bf53b4db470706bbc2f6c4557cd
Rumgaz <ramgar@mail.ru>
2014-03-17 22:42:21 +0600

    Fix
    Теперь выводятся все значения, но доходит только длина строки. Что я
    делаю не так?

---
+++ HW_2_Fix/Properties/AssemblyInfo.cs~Fix
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Options")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Options")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("40793c02-35db-4772-9436-61d89570a42b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HW_2_Fix/XtraReport1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Options
+{
+    partial class XtraReport1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+            this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
+            this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
+<<<<<<< HEAD:HW_2_Final/XtraReport1.Designer.cs
+            this.bindingSource3 = new System.Windows.Forms.BindingSource(this.components);
+            this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
+            this.xrLabel2 = new DevExpress.XtraReports.UI.XRLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource3)).BeginInit();
+=======
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+>>>>>>> Fix:HW_2_Final/XtraReport1.Designer.cs
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // Detail
+            // 
+            this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrLabel2,
+            this.xrLabel1});
+            this.Detail.HeightF = 24.16667F;
+            this.Detail.MultiColumn.Layout = DevExpress.XtraPrinting.ColumnLayout.AcrossThenDown;
+            this.Detail.MultiColumn.Mode = DevExpress.XtraReports.UI.MultiColumnMode.UseColumnCount;
+            this.Detail.Name = "Detail";
+            this.Detail.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // TopMargin
+            // 
+            this.TopMargin.Name = "TopMargin";
+            this.TopMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // BottomMargin
+            // 
+            this.BottomMargin.Name = "BottomMargin";
+            this.BottomMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+<<<<<<< HEAD:HW_2_Final/XtraReport1.Designer.cs
+            // bindingSource3
+            // 
+            this.bindingSource3.DataSource = typeof(Options.Form1.userOptions);
+=======
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = typeof(Options.Form1.userOptions);
+>>>>>>> Fix:HW_2_Final/XtraReport1.Designer.cs
+            // 
+            // xrLabel1
+            // 
+            this.xrLabel1.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+<<<<<<< HEAD:HW_2_Final/XtraReport1.Designer.cs
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "windowSize")});
+            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(23.33333F, 25F);
+=======
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "outData.Length")});
+            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
+>>>>>>> Fix:HW_2_Final/XtraReport1.Designer.cs
+            this.xrLabel1.Name = "xrLabel1";
+            this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
+            this.xrLabel1.SizeF = new System.Drawing.SizeF(100F, 23F);
+            this.xrLabel1.Text = "xrLabel1";
+<<<<<<< HEAD:HW_2_Final/XtraReport1.Designer.cs
+            // 
+            // xrLabel2
+            // 
+            this.xrLabel2.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "brightLevel")});
+            this.xrLabel2.LocationFloat = new DevExpress.Utils.PointFloat(123.3333F, 25F);
+            this.xrLabel2.Name = "xrLabel2";
+            this.xrLabel2.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 120F);
+            this.xrLabel2.SizeF = new System.Drawing.SizeF(100F, 23F);
+            this.xrLabel2.Text = "xrLabel2";
+=======
+>>>>>>> Fix:HW_2_Final/XtraReport1.Designer.cs
+            // 
+            // XtraReport1
+            // 
+            this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.Detail,
+            this.TopMargin,
+            this.BottomMargin});
+<<<<<<< HEAD:HW_2_Final/XtraReport1.Designer.cs
+            this.DataSource = this.bindingSource3;
+            this.Version = "13.2";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource3)).EndInit();
+=======
+            this.DataMember = "outData";
+            this.DataSource = this.bindingSource1;
+            this.Version = "13.2";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+>>>>>>> Fix:HW_2_Final/XtraReport1.Designer.cs
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraReports.UI.DetailBand Detail;
+        private DevExpress.XtraReports.UI.TopMarginBand TopMargin;
+        private DevExpress.XtraReports.UI.BottomMarginBand BottomMargin;
+<<<<<<< HEAD:HW_2_Final/XtraReport1.Designer.cs
+        private System.Windows.Forms.BindingSource bindingSource3;
+=======
+        private System.Windows.Forms.BindingSource bindingSource1;
+>>>>>>> Fix:HW_2_Final/XtraReport1.Designer.cs
+        private DevExpress.XtraReports.UI.XRLabel xrLabel1;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel2;
+    }
+}
