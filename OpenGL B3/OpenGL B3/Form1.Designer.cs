cf66739e83effd499d495826cd7cfc6c5f48592b
Rumgaz <ramgar@mail.ru>
2014-04-11 23:07:22 +0600

    WAAAGH!

---
+++ OpenGL B3/OpenGL B3/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace OpenGL_B3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.drawButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.redBar = new System.Windows.Forms.TrackBar();
+            this.blueBar = new System.Windows.Forms.TrackBar();
+            this.greenBar = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.redBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(12, 12);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(374, 262);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 1;
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(515, 217);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(85, 28);
+            this.drawButton.TabIndex = 2;
+            this.drawButton.Text = "Нарисовать";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "ф"});
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(606, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(405, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Хочу треуольник! Сторону каждую по";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(664, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "сделай!";
+            // 
+            // redBar
+            // 
+            this.redBar.Location = new System.Drawing.Point(509, 66);
+            this.redBar.Name = "redBar";
+            this.redBar.Size = new System.Drawing.Size(104, 42);
+            this.redBar.TabIndex = 8;
+            // 
+            // blueBar
+            // 
+            this.blueBar.Location = new System.Drawing.Point(656, 103);
+            this.blueBar.Name = "blueBar";
+            this.blueBar.Size = new System.Drawing.Size(104, 42);
+            this.blueBar.TabIndex = 9;
+            // 
+            // greenBar
+            // 
+            this.greenBar.Location = new System.Drawing.Point(466, 103);
+            this.greenBar.Name = "greenBar";
+            this.greenBar.Size = new System.Drawing.Size(104, 42);
+            this.greenBar.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(405, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Красного, значит,";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(405, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Зелёного";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(576, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Синего, ровно";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(405, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(142, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "И надобно, чтобы чудо сие";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "вертелось чудно.",
+            "вертелось спокойно.",
+            "не двигалалось."});
+            this.comboBox1.Location = new System.Drawing.Point(553, 155);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 286);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.greenBar);
+            this.Controls.Add(this.blueBar);
+            this.Controls.Add(this.redBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.redBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar redBar;
+        private System.Windows.Forms.TrackBar blueBar;
+        private System.Windows.Forms.TrackBar greenBar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+
+    }
+}
+
---
+++ OpenGL B3/OpenGL B3/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace OpenGL_B3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            simpleOpenGlControl1.InitializeContexts();
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            int w = simpleOpenGlControl1.Width;
+            int h = simpleOpenGlControl1.Height;
+            Gl.glViewport(0, 0, w, h);
+            Glu.gluPerspective(90, w / h, 1, 100);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Glu.gluLookAt(0, 0, 5, 0, 0, 1, 0, 1, 0);
+
+            
+        }
+        
+
+        //*****
+        //Программа 1
+        //*****
+
+        public int rotor1=0 , rotor2=0, rotor3=0;
+        void giveRotor()
+        {
+            timer2.Enabled = true;
+            rotor1 = rotor2 = rotor3 = 0;
+            var ofd = new OpenFileDialog();
+            var file = File.Open(fileName, FileMode.Open);
+            var xs = new XmlSerializer(typeof(TrianglesOptions));
+            var triaglesOptionOUT = (TrianglesOptions)xs.Deserialize(file);
+            file.Close();
+
+            if (triaglesOptionOUT.rotor == 0)
+                rotor1 = rotor2 = rotor3 = 1;
+            if (triaglesOptionOUT.rotor == 1)
+                rotor1 = 1;
+            if (triaglesOptionOUT.rotor == 2)
+                timer2.Enabled = false;
+        }
+
+
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+           // giveRotor();           
+            
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glPushMatrix();           
+            
+            Gl.glRotated(andgle, rotor1, rotor2, rotor3);
+            
+            Gl.glBegin(Gl.GL_TRIANGLES);
+
+            var ofd = new OpenFileDialog();
+            var file = File.Open(fileName, FileMode.Open);
+            var xs = new XmlSerializer(typeof(TrianglesOptions));
+            var triaglesOptionOUT = (TrianglesOptions)xs.Deserialize(file);
+            file.Close();
+
+
+            Gl.glColor3f(triaglesOptionOUT.color[0], 0, 0);
+            Gl.glVertex3f(-triaglesOptionOUT.side, 0, 0);
+
+            Gl.glColor3f(0, triaglesOptionOUT.color[1], 0);
+            Gl.glVertex3f(triaglesOptionOUT.side, 0, 0);
+
+            Gl.glColor3f(0, 0, triaglesOptionOUT.color[2]);
+            Gl.glVertex3f(0, triaglesOptionOUT.side, 0);
+
+            Gl.glEnd();
+
+            Gl.glPopMatrix();
+        }
+
+
+        public double andgle = 0;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            simpleOpenGlControl1.Invalidate();
+            andgle += 10;   
+        }
+
+
+        //*****
+        //Программа 2
+        //*****
+        string fileName = "trianglesOptions";
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            //Сохранение
+            var sfd = new SaveFileDialog();   
+                var trianglesOptionsIN = new TrianglesOptions();
+                trianglesOptionsIN.side = Convert.ToInt32(textBox1.Text);
+                trianglesOptionsIN.color.Add(redBar.Value);
+                trianglesOptionsIN.color.Add(greenBar.Value);
+                trianglesOptionsIN.color.Add(blueBar.Value);
+                trianglesOptionsIN.rotor = whatToDo();
+            var xs = new XmlSerializer(typeof(TrianglesOptions));            
+            var fileStream = File.Create(fileName);
+            xs.Serialize(fileStream, trianglesOptionsIN);
+            fileStream.Close();         
+
+            giveRotor();
+        }
+        //Задание треугольника
+        public class TrianglesOptions
+        {
+            public int side { get; set; }
+            public List<float> color = new List<float>();
+
+            public float rotor { get; set; }
+            
+        }
+        //Способ закрутки
+        int whatToDo()
+        {
+            
+            if (comboBox1.Text == "вертелось чудно.")
+                return 0;
+            if (comboBox1.Text == "вертелось спокойно.")
+                return 1;
+                return 2;
+        }
+        //В textbox1 вводим только цифры
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar)))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }           
+
+    }
+}
